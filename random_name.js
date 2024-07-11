60f6933d4cdbd04ebe0f8cf267dbce5236069517
Prajwal G Bhadrannavar <152140662+Prajwal-Bhadrannavar@users.noreply.github.com>
2024-07-11 17:58:04 +0530

    Create random_name.js

---
+++ random_name.js
@@ -0,0 +1,10 @@
+function whosPaying(names) {
+ var number = (names.length);
+ var lucky = Math.floor(Math.random() * number);
+ 
+ 
+   var result = (names[lucky] +" is going to buy lunch today!"); 
+    
+return result;
+ 
+}
